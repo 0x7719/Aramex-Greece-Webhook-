a41806383824d51d43d21ba2e3684dfb7325202c
0x7719 <tlavranos@gmail.com>
2026-06-13 19:09:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 app = FastAPI()
+
 
 @app.get("/")
 def home():
     return {
         "status": "ok",
         "message": "Aramex Greece Webhook Online"
+    }
+
+
+@app.post("/webhook/daily")
+async def daily_webhook(request: Request):
+
+    payload = await request.json()
+
+    print("RECEIVED:")
+    print(payload)
+
+    return {
+        "received": True,
+        "payload": payload
     }
