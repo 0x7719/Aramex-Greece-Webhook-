8c9d698cf1440c1f82cc3ad7aa3a393f475704ba
0x7719 <tlavranos@gmail.com>
2026-06-13 19:16:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -13,13 +13,32 @@ def home():
 
 @app.post("/webhook/daily")
 async def daily_webhook(request: Request):
-
     payload = await request.json()
 
-    print("RECEIVED:")
-    print(payload)
+    shipment_number = payload.get("shipment_number")
+    awb = payload.get("ext_number")
+
+    data = payload.get("data", {})
+    status_id = data.get("status_id")
+    status_name = data.get("status_name")
+    driver_name = data.get("driver_name")
+    comments = data.get("comments")
+    timestamp = data.get("timestamp")
+
+    print("DAILY WEBHOOK RECEIVED")
+    print("Shipment Number:", shipment_number)
+    print("AWB:", awb)
+    print("Status ID:", status_id)
+    print("Status Name:", status_name)
+    print("Driver:", driver_name)
+    print("Comments:", comments)
+    print("Timestamp:", timestamp)
 
     return {
         "received": True,
-        "payload": payload
+        "shipment_number": shipment_number,
+        "awb": awb,
+        "status_id": status_id,
+        "comments": comments,
+        "timestamp": timestamp
     }
